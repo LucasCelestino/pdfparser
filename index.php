47ce4399443ee593b5bd985f85495cc4f17fc757
Lucas Celestino <lucascap899@gmail.com>
2023-10-18 15:20:33 -0300

    testes

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 
+require("vendor/autoload.php");
+
 // Parse PDF file and build necessary objects.
 $parser = new \Smalot\PdfParser\Parser();
-$pdf = $parser->parseFile('/path/to/document.pdf');
+$pdf = $parser->parseFile('cronograma-semestral.pdf');
 
 $text = $pdf->getText();
-echo $text;
+
+$explodedText = explode("- ", $text);
+
+// removendo header
+unset($explodedText[0]);
+unset($explodedText[1]);
+
+array_pop($explodedText);
+
+foreach ($explodedText as $key => $value)
+{
+    echo $value;
+    echo "<hr>";
+}
