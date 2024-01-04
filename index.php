1dc6418a6f251942e48e390741af217325e0caf0
Lucas Celestino <lucascap899@gmail.com>
2024-01-04 00:07:13 -0300

    ajustando strings que são aceitas na condição de 'a ' (excluindo 4 ultimos caracteres)

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ $pdf = $parser->parseFile('cronograma-semestral.pdf');
 $pattern = '/\d{2} - .+/';
 $data = array();
 $finalData = array();
+$finalres = array();
+$segundoSemestreMeses = ['julho','agosto','setembro','outubro','novembro','dezembro'];
+$res = array();
 
 foreach ($pdf->getPages() as $page) {
 
@@ -22,7 +25,7 @@ foreach ($pdf->getPages() as $page) {
 // tirando espaços duplicados
 foreach ($data as $key => $value)
 {
-    $data[$key] =  str_replace('	', '', $value);;
+    $data[$key] =  str_replace('	', '', $value);
 }
 
 
@@ -35,4 +38,50 @@ foreach ($data as $key => $value)
     $finalData[$key] = $matchs[0];
 }
 
+foreach ($finalData as $string => $text) {
+    $pattern = '/(?=\d{2} -)/';
+    $res = preg_split($pattern, $text[0]);
+    foreach ($res as $key => $value)
+    {
+        $finalres[] = $value;
+    }
 
+}
+
+// Problema: 4=>09 - Dia da Revolução Constitucionalista 10 a
+// --------  5=>31 - Recesso Escolar
+
+// Jeito Correto: 4=>09 - Dia da Revolução Constitucionalista
+// -------------  5=>10 a 31 - Recesso Escolar
+
+
+foreach ($finalres as $key => $value)
+{
+    $standard = substr($value, -2);
+
+    if($standard == "a ")
+    {
+        $finalres[$key] = substr($value, 0, -5);
+    }
+}
+
+
+foreach ($finalres as $key => $value)
+{
+    echo $key.'=>'.$value;
+    echo '<br>';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
